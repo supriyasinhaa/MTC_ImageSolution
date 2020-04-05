5bd054f44198653387ca1df5b7088c7c8ff6a06f
supriyasinhaa <47394534+supriyasinhaa@users.noreply.github.com>
2020-04-05 17:48:46 +0530

    Add files via upload

---
+++ displayupload/imgupload.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 05, 2020 at 01:57 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `displayupload`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imgupload`
+--
+
+CREATE TABLE `imgupload` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `imgupload`
+--
+
+INSERT INTO `imgupload` (`id`, `username`, `image`) VALUES
+(1, 'Nature', 'upload/101.jpg'),
+(2, 'Flower', 'upload/105.jpg'),
+(3, 'Leaves', 'upload/104.jpg'),
+(4, 'Water', 'upload/103.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `imgupload`
+--
+ALTER TABLE `imgupload`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `imgupload`
+--
+ALTER TABLE `imgupload`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
